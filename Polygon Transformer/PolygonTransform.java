ccab7ffcf01fc492662988c65df2b17b27370564
jimmyMsh <103710156+jimmyMsh@users.noreply.github.com>
2023-03-22 23:34:52 -0400

    Update PolygonTransform.java

--- Polygon Transformer/PolygonTransform.java
+++ Polygon Transformer/PolygonTransform.java
@@ -53,5 +53,15 @@ public class PolygonTransform {
 
     // Tests each of the API methods by directly calling them. 
     public static void main(String[] args) {
+	    // Scales polygon by the factor 2.
+    StdDraw.setScale(-5.0, +5.0); 
+    double[] x = { 0, 1, 1, 0 }; 
+    double[] y = { 0, 0, 2, 1 }; 
+    double alpha = 2.0; 
+    StdDraw.setPenColor(StdDraw.RED); 
+    StdDraw.polygon(x, y); 
+    scale(x, y, alpha); 
+    StdDraw.setPenColor(StdDraw.BLUE);
+    StdDraw.polygon(x, y);
     }
 }
